6be55b8ca9d01a38a3b3dd9222427f30d467d470
baharudin78 <baharudinmaulana78@gmail.com>
2022-06-21 20:41:36 +0700

    twice

--- app/src/main/java/com/baharudin/pengaduanapp/ui/success/SuccesActivity.kt
+++ app/src/main/java/com/baharudin/pengaduanapp/ui/success/SuccesActivity.kt
@@ -1,14 +1,24 @@
 package com.baharudin.pengaduanapp.ui.success
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.baharudin.pengaduanapp.R
+import com.baharudin.pengaduanapp.databinding.ActivitySuccesBinding
+import com.baharudin.pengaduanapp.ui.home.HomeActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SuccesActivity : AppCompatActivity() {
+    private lateinit var binding : ActivitySuccesBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_succes)
+        binding = ActivitySuccesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnBack.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+            finish()
+        }
     }
 }
